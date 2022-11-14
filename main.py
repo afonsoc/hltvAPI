79dfd0bb427ea4c277ffd5f320c92009191d6910
Afonso Cabecadas <afonsomcab@gmail.com>
2022-11-14 10:15:54 +0000

    refactor player dict

--- main.py
+++ main.py
@@ -14,21 +14,18 @@ for player in allPlayers:
     # testvar = player.text
     if player.find("a") != None:
         playerArray.append({'Name': player.find("a").text,
-                'KD': player.find("td", class_ = "statsDetail").text, 
-                'Rating': player.find("td", class_ = "ratingCol").text})
+                'stats':{'KD': player.find("td", class_ = "statsDetail").text, 
+                'Rating': player.find("td", class_ = "ratingCol").text}})
     
 
 # print(len(playerArray))
 # print(player.text)
 
 print(type(playerArray[-1]))
+json.dumps(playerArray, indent = 2)
+        
 
 for i in playerArray:
     print(i)
 print(len(playerArray))
 print(type(playerArray[-1]))
-
-test = json.dumps(playerArray[-1])
-
-print(type(test))
-        

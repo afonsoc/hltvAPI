bfc30877d8b7db9a13143aef012b3495828f5cb7
Afonso Cabecadas <afonsomcab@gmail.com>
2022-11-14 10:34:05 +0000

    added requirements and refactored code

--- main.py
+++ main.py
@@ -11,21 +11,11 @@ allPlayers = soup.find_all("tr")
 playerArray = []
 
 for player in allPlayers:
-    # testvar = player.text
     if player.find("a") != None:
-        playerArray.append({'Name': player.find("a").text,
+        playerArray.append(json.dumps({'Name': player.find("a").text,
                 'stats':{'KD': player.find("td", class_ = "statsDetail").text, 
-                'Rating': player.find("td", class_ = "ratingCol").text}})
-    
-
-# print(len(playerArray))
-# print(player.text)
-
-print(type(playerArray[-1]))
-json.dumps(playerArray, indent = 2)
-        
+                'Rating': player.find("td", class_ = "ratingCol").text}}, indent = 2))
 
 for i in playerArray:
     print(i)
 print(len(playerArray))
-print(type(playerArray[-1]))

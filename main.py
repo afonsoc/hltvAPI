2d20252bf2b25b3729182555bc0d5776f0ea3e92
Afonso Cabecadas <afonsomcab@gmail.com>
2022-11-26 18:49:17 +0000

    initial findByName dev

--- main.py
+++ main.py
@@ -12,10 +12,33 @@ playerArray = []
 
 for player in allPlayers:
     if player.find("a") != None:
-        playerArray.append(json.dumps({'Name': player.find("a").text,
-                'stats':{'KD': player.find("td", class_ = "statsDetail").text, 
-                'Rating': player.find("td", class_ = "ratingCol").text}}, indent = 2))
+        playerArray.append({'name': player.find("a").text,
+                'link': player.find("a")["href"],
+                'stats':{'KD': player.find("td", class_ = "statsDetail").text,
+                'Rating': player.find("td", class_ = "ratingCol").text}})
 
 for i in playerArray:
     print(i)
 print(len(playerArray))
+
+testname = "OCEAN"
+
+def findByName(playerName):
+    playerStats = {}
+    relevantStats = ["Total kills", "Headshot %", "Total deaths", "K/D Ratio",
+                     "Damage / Round", "Grenade dmg / Round", "Maps played", "Rounds played", "Kills / round",
+                     "Assists / round", "Deaths / round", "Saved by teammate / round", "Saved teammates / round",
+                     "Rating 2.0"]
+    for player in playerArray:
+        if player['name'] == playerName:
+            playerPage = requests.get("https://www.hltv.org" + player['link'])
+            playerSoup = BeautifulSoup(playerPage.text, "lxml")
+
+            allSpans = playerSoup.find_all("span")
+            for item in allSpans:
+                if item.text in relevantStats:
+                    playerStats.update({item.text: item.find_next("span").text})
+    return playerStats
+
+print(findByName(testname))
+

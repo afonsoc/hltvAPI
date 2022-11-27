3654c14d388fe3ef01f2d54b38ff1d5ec82c471e
Afonso Cabecadas <afonsomcab@gmail.com>
2022-11-27 12:38:20 +0000

    minor code refactor

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-requestedPage = requests.get("https://www.hltv.org/stats/players")
-soup = BeautifulSoup(requestedPage.text, "lxml")
+def parsePage(url):
+    return BeautifulSoup(requests.get(url).text, "lxml")
+
+soup = parsePage("https://www.hltv.org/stats/players")
 
 allPlayers = soup.find_all("tr")
 
@@ -13,15 +15,13 @@ playerArray = []
 for player in allPlayers:
     if player.find("a") != None:
         playerArray.append({'name': player.find("a").text,
-                'link': player.find("a")["href"],
-                'stats':{'KD': player.find("td", class_ = "statsDetail").text,
-                'Rating': player.find("td", class_ = "ratingCol").text}})
+                'link': player.find("a")["href"]})
 
 for i in playerArray:
     print(i)
 print(len(playerArray))
 
-testname = "OCEAN"
+testname = "FoX"
 
 def findByName(playerName):
     playerStats = {}
@@ -29,16 +29,16 @@ def findByName(playerName):
                      "Damage / Round", "Grenade dmg / Round", "Maps played", "Rounds played", "Kills / round",
                      "Assists / round", "Deaths / round", "Saved by teammate / round", "Saved teammates / round",
                      "Rating 2.0"]
+
     for player in playerArray:
-        if player['name'] == playerName:
-            playerPage = requests.get("https://www.hltv.org" + player['link'])
-            playerSoup = BeautifulSoup(playerPage.text, "lxml")
+        if player['name'].upper() == playerName.upper():
+            playerSoup = parsePage("https://www.hltv.org" + player['link'])
 
             allSpans = playerSoup.find_all("span")
-            for item in allSpans:
-                if item.text in relevantStats:
-                    playerStats.update({item.text: item.find_next("span").text})
-    return playerStats
+            for span in allSpans:
+                if span.text in relevantStats:
+                            playerStats.update({span.text.title().replace(" ", ""): span.find_next("span").text})
+            return json.dumps(playerStats, indent=1)
 
 print(findByName(testname))
 

dbbbb42038b041c4e6dc68417e1e374a5cdb4406
Afonso Cabecadas <afonsomcab@gmail.com>
2022-11-27 20:25:00 +0000

    added error handling

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ for i in playerArray:
     print(i)
 print(len(playerArray))
 
-testname = "ocean"
+testname = "fox"
 
 def findByName(playerName):
     playerStats = {}
@@ -31,14 +31,19 @@ def findByName(playerName):
                      "Rating 1.0"]
 
     for player in playerArray:
-        if player['name'].upper() == playerName.upper():
-            playerSoup = parsePage("https://www.hltv.org" + player['link'])
+        if type(playerName) != str:
+            raise AttributeError("Argument %s needs to be a string" % repr(playerName))
 
+        elif player['name'].upper() == playerName.upper():
+            playerSoup = parsePage("https://www.hltv.org" + player['link'])
+            
             allSpans = playerSoup.find_all("span")
             for span in allSpans:
                 if span.text in relevantStats:
                             playerStats.update({span.text.title().replace(" ", ""): span.find_next("span").text})
             return json.dumps(playerStats, indent=1)
+    else:
+        raise ValueError("Player %s not found" % repr(playerName))
 
 print(findByName(testname))
 

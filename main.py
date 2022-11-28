6630a2104e97e337ecc7ca57302170a73d8c2275
Afonso Cabecadas <afonsomcab@gmail.com>
2022-11-28 10:50:00 +0000

    finish get player by name

--- main.py
+++ main.py
@@ -5,38 +5,36 @@ from bs4 import BeautifulSoup
 def parsePage(url):
     return BeautifulSoup(requests.get(url).text, "lxml")
 
-soup = parsePage("https://www.hltv.org/stats/players")
+def findPlayerByName(playerName):
 
-allPlayers = soup.find_all("tr")
-
-
-playerArray = []
-
-for player in allPlayers:
-    if player.find("a") != None:
-        playerArray.append({'name': player.find("a").text,
-                'link': player.find("a")["href"]})
-
-for i in playerArray:
-    print(i)
-print(len(playerArray))
-
-testname = "fox"
-
-def findByName(playerName):
+    #Initiate data structures
     playerStats = {}
+    playerArray = []
     relevantStats = ["Total kills", "Headshot %", "Total deaths", "K/D Ratio",
                      "Damage / Round", "Grenade dmg / Round", "Maps played", "Rounds played", "Kills / round",
                      "Assists / round", "Deaths / round", "Saved by teammate / round", "Saved teammates / round",
                      "Rating 1.0"]
 
+    #Get parsed player page
+    EveryPlayerSoup = parsePage("https://www.hltv.org/stats/players")
+    allPlayers = EveryPlayerSoup.find_all("tr")
+
+    #Get players names and links to overview stats page
+    for player in allPlayers:
+        if player.find("a") != None:
+            playerArray.append({'name': player.find("a").text,
+                    'link': player.find("a")["href"]})
+
+
     for player in playerArray:
         if type(playerName) != str:
             raise AttributeError("Argument %s needs to be a string" % repr(playerName))
 
+        #check if the incoming player request matches value in list
         elif player['name'].upper() == playerName.upper():
             playerSoup = parsePage("https://www.hltv.org" + player['link'])
             
+            #Get list of stats if above check was successful
             allSpans = playerSoup.find_all("span")
             for span in allSpans:
                 if span.text in relevantStats:
@@ -44,6 +42,4 @@ def findByName(playerName):
             return json.dumps(playerStats, indent=1)
     else:
         raise ValueError("Player %s not found" % repr(playerName))
-
-print(findByName(testname))
 
